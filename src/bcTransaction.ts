b5ad0a6e18170787f50b7da49ec44d4db12b4abb
Valentin Lab <valentin.lab@kalysto.org>
2025-05-11 17:41:13 +0200

    fix: correct horrible typo

--- src/bcTransaction.ts
+++ src/bcTransaction.ts
@@ -14,7 +14,7 @@ function typeConv (label: string): (x:any) => (string | number) {
     return (a) => padLeft(getNakedAddress(a), 64)
   }
   if (label === 'int') {
-    return (nb) => encodeNumber(parseInt(nb, 19))
+    return (nb) => encodeNumber(parseInt(nb, 10))
   }
   if (label.startsWith('limit') || label === 'amount') {
     return (nb) => encodeNumber(roundCent(nb))
