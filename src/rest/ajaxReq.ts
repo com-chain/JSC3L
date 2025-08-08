ab5a3a8994b2b804606aa9e1cf0a3ff14e6ad8f0
Valentin Lab <valentin.lab@kalysto.org>
2025-08-08 06:49:16 +0200

    new: [ajaxReq] make ``ethCallAt`` calls be automatically batched and deduplicated

--- src/rest/ajaxReq.ts
+++ src/rest/ajaxReq.ts
@@ -12,6 +12,14 @@ class URL {
   static requestMessages = 'requestMessages.php';
 }
 
+function cmpEthCallAt(d1, d2) {
+  return (d1.ethCallAt.data === d2.ethCallAt.data &&
+    d1.ethCallAt.to === d2.ethCallAt.to &&
+    d1.blockNb === d2.blockNb)
+}
+
+const postPromises = new WeakMap()
+
 
 export default abstract class AjaxReqAbstract {
 
@@ -19,32 +27,117 @@ export default abstract class AjaxReqAbstract {
   // ``jsc3l.wallet``)
   public abstract endpoint: any
 
-  pendingPosts = []
+  pendingPosts: {
+    data: any,
+    resolve: Function | Function[],
+    reject: Function | Function[],
+  }[] = []
 
   //
   // URL.SERVER POST requests (that are using queuing mecanism)
   //
 
   post (data) {
-    const self = this
-    return new Promise(function (resolve, reject) {
-      self.pendingPosts.push({ data, resolve, reject })
-      if (self.pendingPosts.length === 1) self.queuePost()
+    const { queuePost, pendingPosts } = this
+    const bindQueuePost = queuePost.bind(this)
+    // remove duplicate
+    for (const [idx, req] of pendingPosts.entries()) {
+      if (req.data.hasOwnProperty("ethCallAt")) {
+        if (typeof req.reject !== "function") {
+          throw new Error("Unexpected value")
+        }
+        if (typeof req.resolve !== "function") {
+          throw new Error("Unexpected value")
+        }
+        if (cmpEthCallAt(data, req.data)) {
+          return postPromises.get(req.data)
+        }
+        break
+      }
+      if (req.data.hasOwnProperty("batch")) {
+        for (const reqData of req.data.batch) {
+          if (cmpEthCallAt(data, reqData)) {
+            return postPromises.get(reqData)
+          }
+        }
+        break
+      }
+    }
+
+    const promise = new Promise(function (resolve, reject) {
+      let added = false
+      if (data.hasOwnProperty("ethCallAt")) {
+        // look for other to aggregate
+        for (const [idx, req] of pendingPosts.entries()) {
+          if (req.data.hasOwnProperty("ethCallAt")) {
+            if (typeof req.reject !== "function") {
+              throw new Error("Unexpected value")
+            }
+            if (typeof req.resolve !== "function") {
+              throw new Error("Unexpected value")
+            }
+            // bingo, let's transform it to a batch
+            pendingPosts[idx] = {
+              data: { batch: [req.data, data] },
+              resolve: [req.resolve, resolve],
+              reject: [req.reject, reject],
+            }
+            added = true
+            break
+          }
+          if (req.data.hasOwnProperty("batch")) {
+            (req.data.batch as Function[]).push(data);
+            (req.resolve as Function[]).push(resolve);
+            (req.reject as Function[]).push(reject)
+            added = true
+            break
+          }
+        }
+      }
+      if (!added) {
+        pendingPosts.push({ data, resolve, reject })
+      }
+
+      // give the handle to potential other deferred
+      setTimeout(bindQueuePost, 0)
     })
+    postPromises.set(data, promise)
+    return promise
   }
 
-  queuePost () {
-    const { data, resolve, reject } = this.pendingPosts[0]
-
-    this.endpoint.post(URL.SERVER, data).then(res => {
-      if (res.error) {
-        reject(new APIError(res.msg, res.data))
-        return
+  private async queuePost () {
+    const posts = this.pendingPosts.splice(0)  // empties pendingPosts
+    if (posts.length == 0) return
+    await Promise.allSettled(posts.map(
+      async ({data, resolve, reject}) => {
+        // patching batch
+        if (data.hasOwnProperty("batch")) {
+          data.batch = data.batch.map((x: any) => ({
+            ethCall: x.ethCallAt,
+            blockNb: x.blockNb,
+          }))
+        }
+        const res = await this.endpoint.post(URL.SERVER, data)
+        if (res.error) {
+          if (!Array.isArray(reject)) reject = [reject]
+          for (const r of reject) {
+            r(new APIError(res.msg, res.data))
+          }
+          return
+        }
+        if (Array.isArray(resolve)) {
+          for (const [idx, subRes] of res.data.entries()) {
+            if (subRes.error) {
+              reject[idx](new APIError(subRes.error.msg, subRes.data))
+            } else {
+              resolve[idx](subRes.data)
+            }
+          }
+          return
+        }
+        resolve(res.data)
       }
-      resolve(res.data)
-    })
-    this.pendingPosts.splice(0, 1)
-    if (this.pendingPosts.length > 0) { this.queuePost() }
+    ))
   }
 
 
